fae33d03c57763916493f9974cbab06da4f1596e
MarlonStefano <131677187+MarlonStefano@users.noreply.github.com>
2024-01-25 20:35:28 -0300

    Update ex_C4xC2.py

--- ex_C4xC2.py
+++ ex_C4xC2.py
@@ -1,3 +1,3 @@
 F = pAdicField( 2, 3, print_mode = "digits" )
-G = AbelianGroup( [4,2] )
+G = AbelianGroupGap( [4,2] )
 mats =[matrix(F,[[1,0,0],[-1,-1,-2],[0,1,1] ]), matrix(F,[[1,0,0],[0,1,0],[0,0,1]])]
