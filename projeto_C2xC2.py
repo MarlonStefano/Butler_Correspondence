b574bfddf7cc8f4f29bf75cbfe2f767484bc67c0
MarlonStefano <131677187+MarlonStefano@users.noreply.github.com>
2024-02-07 16:31:43 -0300

    Update projeto_C2xC2.py

--- projeto_C2xC2.py
+++ projeto_C2xC2.py
@@ -7,27 +7,18 @@ from sage.groups.abelian_gps.abelian_group_gap import AbelianGroupGap
 def Diag_C2xC2(k):
     V=AbelianGroupGap([2]*k)
     sub=V.all_subgroups()
-    L2=[[] for _ in range(len(sub)) ]
-    for i in range(len(sub)):
-        L2[i]=[[sub[i],sub[j]] for j in range(len(sub))]
-    L3=[[] for _ in range(len(sub))]
-    for i in range(len(sub)):
-        L3[i]=[[L2[i][j]+[sub[k]] for j in range(len(sub)) ] for k in range(len(sub))] 
-    L4=[[] for _ in range(len(sub))]
-    for i in range(len(sub)):
-        L4[i]=[[[L3[i][k][j]+[sub[m]] for k in range(len(sub))] for j in range(len(sub))] for m in range(len(sub))]    
-    L4=sum(sum(sum(L4, [] ), []), [])
+    L=[[x,y,z,w] for x in sub for y in sub for z in sub for w in sub]
 
-    di_gen=[[] for _ in range(len(L4))]
-    gen_che=[[] for _ in range(len(L4))]
-    for i in range(len(L4)):
-        di_gen[i]=[[x for x in L4[i][j].gens() ] for j in range(4)]
+    di_gen=[[] for _ in range(len(L))]
+    gen_che=[[] for _ in range(len(L))]
+    for i in range(len(L)):
+        di_gen[i]=[[x for x in 4[i][j].gens() ] for j in range(4)]
         gen_che[i]=[[di_gen[i][j] for j in range(4) if j!=m] for m in range(4)]
         gen_che[i]=[sum(gen_che[i][j],[]) for j in range(4)]
     diag=[ ]
-    for i in range(len(L4)): 
+    for i in range(len(L)): 
         if all(V.subgroup(gen_che[i][j]).order()==V.order() for j in range(4)):
-           diag.append(L4[i])          
+           diag.append(L[i])          
     gen_di=[[diag[i][j].gens() for j in range(4)] for i in range(len(diag))]
     gen_Vi=[[[ V(x).exponents() for x in gen_di[i][j]] for j in range(4)] for i in range(len(diag))]
     for j in range(len(gen_Vi)):
