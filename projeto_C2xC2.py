c364d15b91917e101732fb34f08a76271cf03ebf
MarlonStefano <131677187+MarlonStefano@users.noreply.github.com>
2024-02-10 18:34:11 -0300

    Update projeto_C2xC2.py

--- projeto_C2xC2.py
+++ projeto_C2xC2.py
@@ -22,28 +22,31 @@ def Diag_C2xC2(k):
     for i in range(len(L)): 
         if all(V.subgroup(gen_che[i][j]).order()==2**k for j in range(4)):
            diag.append(L[i])
-    cl=[[] for _ in range(len(diag))]
-    for i in range(1,len(diag)):
-        cl[0]=[x for x in diag if any(all(V.subgroup([f(diag[0][j].gens()[l]) for l in range(len(diag[0][j].gens()))]).is_subgroup_of(x[j])  and V.subgroup([f(diag[0][j].gens()[l]) for l in range(len(diag[0][j].gens()))]).order()==x[j].order() for j in range(4) ) for f in aut)]
-        cl[i]=[x for x in diag if any(all(V.subgroup([f(diag[i][j].gens()[l]) for l in range(len(diag[i][j].gens()))]).is_subgroup_of(x[j])  and V.subgroup([f(diag[i][j].gens()[l]) for l in range(len(diag[i][j].gens()))]).order()==x[j].order() for j in range(4) ) for f in aut) and all(x not in cl[k] for k in range(i)) ]
-    cl=[x for x in cl if x!=[]] 
-    cl=[cl[j][0] for j in range(len(cl))]
-
-    gen_di=[[cl[i][j].gens() for j in range(4)] for i in range(len(cl))]
-    gen_Vi=[[[ V(x).exponents() for x in gen_di[i][j]] for j in range(4)] for i in range(len(cl))]
+    gen_di=[[diag[i][j].gens() for j in range(4)] for i in range(len(diag))]
+    gen_Vi=[[[ V(x).exponents() for x in gen_di[i][j]] for j in range(4)] for i in range(len(diag))]
     for j in range(len(gen_Vi)):
         for i in range(4):
             if len(gen_Vi[j][i])==0:
                 gen_Vi[j][i]=[vector([0 for j in range(k)])]
+    pro=[x for x in M if x**2==x]
+    pro_diag=[[x for x in pro if  all(all(x*W(w) in W.subspace([y for y in gen_Vi[i][j]]) for w in gen_Vi[i][j]) for j in range(4)) ] for i in range(len(gen_Vi))   ]
+    idecom=[gen_Vi[i] for i in range(len(gen_Vi)) if len(pro_diag[i])==2]
+    idecom_gro=[diag[gen_Vi.index(x)] for x in idecom] 
+
+    cl=[[] for _ in range(len(idecom_gro))]
+    for i in range(1,len(idecom_gro)):
+        cl[0]=[x for x in idecom_gro if any(all(V.subgroup([f(idecom_gro[0][j].gens()[l]) for l in range(len(idecom_gro[0][j].gens()))]).is_subgroup_of(x[j])  and V.subgroup([f(idecom_gro[0][j].gens()[l]) for l in range(len(idecom_gro[0][j].gens()))]).order()==x[j].order() for j in range(4) ) for f in aut)]
+        cl[i]=[x for x in idecom_gro if any(all(V.subgroup([f(idecom_gro[i][j].gens()[l]) for l in range(len(idecom_gro[i][j].gens()))]).is_subgroup_of(x[j])  and V.subgroup([f(idecom_gro[i][j].gens()[l]) for l in range(len(idecom_gro[i][j].gens()))]).order()==x[j].order() for j in range(4) ) for f in aut) and all(x not in cl[k] for k in range(i)) ]
+    cl=[x for x in cl if x!=[]] 
+    cl=[cl[j][0] for j in range(len(cl))]
+    cl_gen_Vi=[[[ V(x).exponents() for x in cl[i][j].gens()] for j in range(4)] for i in range(len(cl))]
+    
     mats=[[] for _ in range(len(gen_Vi))]
     for i in range(len(gen_Vi)):
         mats[i]=[[identity_matrix(IntegerModRing(2),len(gen_Vi[i][j])),identity_matrix(IntegerModRing(2),len(gen_Vi[i][j]))] for j in range(4)]
-    pro=[x for x in M if x**2==x]
-    pro_diag=[[x for x in pro if  all(x*W(w) in W.subspace([y for y in gen_Vi[i][j]]) for w in gen_Vi[i][j] for j in range(4)) ] for i in range(len(gen_Vi))   ]
-
 
                                          
-    return diag, gen_Vi, mats,cl,pro_diag
+    return diag, gen_Vi, mats,cl,pro_diag,idecom,cl_gen_Vi
 def lattices_C2xC2(k):
     d=Diag_C2xC2(k)
     mats=d[2]
